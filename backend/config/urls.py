83379d6f641685bcb61a8a0ff06f83b8b4d4daa0
Anish singh <anishsinghjsr9546@gmail.com>
2026-09-08 20:07:56 +0530

    Fix production media serving

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,10 @@
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.http import JsonResponse
 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 
 def health_check(request):
@@ -43,8 +44,17 @@ urlpatterns = [
 ]
 
 
-# Media files
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# =========================================================
+# MEDIA FILES
+# =========================================================
+
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
+
